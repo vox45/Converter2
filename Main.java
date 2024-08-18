ffc36e98f4f2f58a682a598f0a7dc7e7884bf3e2
vox45 <golovkodave19@gmail.com>
2024-08-18 16:47:02 +0300

    Added Fahrenheit to Celsius conversion

--- Main.java
+++ Main.java
@@ -3,4 +3,8 @@ public class Main {
         System.out.println("Проект Converter");
         System.out.println("Версія 1.0");
     }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
 }
